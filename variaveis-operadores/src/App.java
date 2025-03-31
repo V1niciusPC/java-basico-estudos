e0025d66a9e9f27faf414ce6bb0b840d9cbb76a8
vinicius <viniciuspereirac3@gmail.com>
2025-03-31 13:33:09 -0400

    commi2 atualização anotações variaveis/operadores

--- variaveis-operadores/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        short idade = 123;
-        System.out.println("Hello, World!");
-        System.out.println(idade);
-    }
-}
